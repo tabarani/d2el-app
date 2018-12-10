3060055c8ca26400a6210e24df623b0f2eff2cbe
wailabualela <wailabualela@gmail.com>
2018-12-10 16:42:15 +0200

    change the app id

--- desktop/electron.js
+++ desktop/electron.js
@@ -96,7 +96,7 @@ fs.readFile(path.join(__dirname, 'config.json'), 'utf8', (err, data) => {
 
     // Default values.
     var ssoScheme = 'moodlemobile',
-        appId = 'com.moodle.moodlemobile';
+        appId = 'sd.d2el.elearning';
 
     if (!err) {
         try {
